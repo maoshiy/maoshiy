b0f3aef7787c2cbfa20609d79454319f5f29fa3f
maoshiy <31596958+maoshiy@users.noreply.github.com>
2020-05-28 20:05:12 +0800

    Update docker-centos.sh

--- docker-centos.sh
+++ docker-centos.sh
@@ -10,16 +10,16 @@ yum remove -y docker \
   docker-engine-selinux \
   docker-engine
 yum install -y yum-utils \
-  evice-mapper-persistent-data \
+  device-mapper-persistent-data \
   lvm2
 yum-config-manager \
   --add-repo \
   https://download.docker.com/linux/centos/docker-ce.repo
-yum-config-manager --enable docker-ce-nightly;yum makecache fast;yum install -y docker-ce;systemctl enable docker;systemctl start docker;usermod -aG docker $USER
+yum-config-manager --enable docker-ce-nightly
+yum makecache fast;yum install -y docker-ce;systemctl enable docker;systemctl start docker;usermod -aG docker $USER;chown "$USER":"$USER" /"$USER"/.docker -R;chmod g+rwx "$HOME/.docker" -R
 sudo tee -a /etc/sysctl.conf <<-EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
 sysctl -p
-chown "$USER":"$USER" /"$USER"/.docker -R;chmod g+rwx "$HOME/.docker" -R
 docker run hello-world
