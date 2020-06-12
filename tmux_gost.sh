6f53bb94ea3c693086bf2aeb518e9ed22d8ee9e4
maoshiy <31596958+maoshiy@users.noreply.github.com>
2020-06-12 22:39:24 +0800

    Update and rename tmux_gost.sh to debian-docker

--- tmux_gost.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-
-nohup gost -L="http://maoshiy:msy@:25" -L="http2://maoshiy:msy@:143?cert=/mnt/gost/cert.pem&key=/mnt/gost/key.pem" -L="socks+tls://maoshiy:msy@:587?cert=/mnt/gost/cert.pem&key=/mnt/gost/key.pem" -L="http+tls://maoshiy:msy@:465?cert=/mnt/gost/cert.pem&key=/mnt/gost/key.pem" -logtostderr -v 5 > /dev/null 2>&1 &
-
